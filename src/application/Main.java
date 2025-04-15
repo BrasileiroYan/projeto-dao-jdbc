0bdbd458d85d9e9784ea73cd26f7397b1619e28d
Yan Pedro <yanpedrobrasileiro@gmail.com>
2025-04-15 20:37:36 -0300

    implementation of findByDepartment method

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,13 @@
 package application;
 
+import model.dao.FactoryDao;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -10,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
@@ -49,20 +52,13 @@ public class SellerDaoJDBC implements SellerDao {
                     + "WHERE seller.Id = ?");
 
             st.setInt(1, id);
+
             rs = st.executeQuery();
 
             if (rs.next()) {
-                Department department = new Department();
-                department.setId(rs.getInt("DeparmentId"));
-                department.setName(rs.getString("DepName"));
+                Department department = instanciateDepartment(rs);
 
-                Seller seller = new Seller();
-                seller.setDepartment(department);
-                seller.setId(rs.getInt("Id"));
-                seller.setName(rs.getString("Name"));
-                seller.setEmail(rs.getString("Email"));
-                seller.setBirthDate(rs.getDate("BirthDate").toLocalDate());
-                seller.setBaseSalary(rs.getDouble("BaseSalary"));
+                Seller seller = instanciateSeller(rs, department);
 
                 return seller;
             }
@@ -78,5 +74,67 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public List<Seller> findAll() {
         return List.of();
+    }
+
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+            "SELECT seller.*,department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE department.Id = ? "
+                    + "ORDER BY Name");
+
+            st.setInt(1, department.getId());
+
+            rs = st.executeQuery();
+
+            List<Seller> sellerList = new ArrayList<>();
+            Map<Integer, Department> departmentMap = new HashMap<>();
+
+            while (rs.next()) {
+                Department dep = departmentMap.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instanciateDepartment(rs);
+                    departmentMap.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller seller = instanciateSeller(rs, dep);
+                sellerList.add(seller);
+            }
+
+            return sellerList;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DbConnection.closeStatement(st);
+            DbConnection.closeResultSet(rs);
+        }
+    }
+
+    private Seller instanciateSeller(ResultSet rs, Department department) throws SQLException {
+        Seller seller = new Seller();
+        seller.setDepartment(department);
+        seller.setId(rs.getInt("Id"));
+        seller.setName(rs.getString("Name"));
+        seller.setEmail(rs.getString("Email"));
+        seller.setBirthDate(rs.getDate("BirthDate").toLocalDate());
+        seller.setBaseSalary(rs.getDouble("BaseSalary"));
+
+        return seller;
+    }
+
+    private Department instanciateDepartment(ResultSet rs) throws SQLException {
+        Department department = new Department();
+        department.setId(rs.getInt("Id"));
+        department.setName(rs.getString("DepName"));
+
+        return department;
     }
 }
