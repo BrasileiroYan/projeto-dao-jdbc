60a5ea61346eceb74481700c260e42ab6a0ea184
Yan Pedro <yanpedrobrasileiro@gmail.com>
2025-04-15 21:07:25 -0300

    method findAll implementation - SellerDaoJDBC

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -61,8 +61,10 @@ public class SellerDaoJDBC implements SellerDao {
                 Seller seller = instanciateSeller(rs, department);
 
                 return seller;
+
             }
             return null;
+
         } catch(SQLException e) {
             throw new DbException(e.getMessage());
         } finally {
@@ -73,7 +75,43 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return List.of();
+
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+
+            st = conn.prepareStatement(
+                "SELECT seller.*,department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "ORDER BY Name");
+
+            rs = st.executeQuery();
+
+            List<Seller> sellerList = new ArrayList<>();
+            Map<Integer, Department> departmentMap = new HashMap<>();
+
+            while (rs.next()) {
+                Department dep = departmentMap.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instanciateDepartment(rs);
+                    departmentMap.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller seller = instanciateSeller(rs, dep);
+                sellerList.add(seller);
+            }
+
+            return sellerList;
+
+        } catch(SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DbConnection.closeStatement(st);
+            DbConnection.closeResultSet(rs);
+        }
     }
 
     @Override
@@ -110,6 +148,7 @@ public class SellerDaoJDBC implements SellerDao {
             }
 
             return sellerList;
+
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
         } finally {
